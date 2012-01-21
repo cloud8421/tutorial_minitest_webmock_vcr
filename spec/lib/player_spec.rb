4111a769a2f71ae02a185c232a07f7c16a38b327
Claudio Ortolina <claudio@jumpzero.com>
2012-01-21 10:38:38 +0000

    Base specs failing

--- spec/lib/player_spec.rb
+++ spec/lib/player_spec.rb
@@ -2,8 +2,16 @@ require_relative '../spec_helper'
 
 describe Dish::Player do
 
-  it "must work" do
-    "Yay!".must_be_instance_of String
+  describe "default attributes" do
+
+    it "must include httparty methods" do
+      Dish::Player.must_include HTTParty
+    end
+
+    it "must have the base url set to the Dribble API endpoint" do
+      Dish::Player.base_uri.must_equal 'http://api.dribbble.com/'
+    end
+
   end
 
 end
